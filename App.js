5d99912136b60bf0877e32d3e5ed2d0858aed3ee
shivrajmachigadkar <159455906+shivrajsm07@users.noreply.github.com>
2024-09-27 00:20:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import Quiz from "./Components/Quiz/Quiz";
+
+function App() {
+  return (
+    <>
+        <Quiz/>
+    </>
+  );
+}
+
+export default App;
---
+++ data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        question : '5 + 10',
+        option1 : '5',
+        option2 : '10',
+        option3 : '15',
+        option4 : '25',
+        ans : '3'
+    },
+    {
+        question : '2 * 20',
+        option1 : '20',
+        option2 : '40',
+        option3 : '60',
+        option4 : '80',
+        ans : '2'
+    },
+    {
+        question : '3 + 89',
+        option1 : '3',
+        option2 : '89',
+        option3 : '92',
+        option4 : '389',
+        ans : '3'
+    },
+    {
+        question : '6 / 3',
+        option1 : '1',
+        option2 : '3',
+        option3 : '2',
+        option4 : '6',
+        ans : '2'
+    },
+    {
+        question : '2 + 2 / 2',
+        option1 : '2',
+        option2 : '4',
+        option3 : '222',
+        option4 : '3',
+        ans : '4'
+    }
+]
